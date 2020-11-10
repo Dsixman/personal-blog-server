e46356e632a5e18906599d1bd67c27b9114ac7ca
zzhaicn <zhengmurong168@sina.com>
2020-11-11 01:16:57 +0800

    服务端的入口文件

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-const koa=require('koa')
-const app=new koa()
-app.listen(3000)
+const Koa=require('koa')
+const path=require('path')
+const koabody=require('koa-body')
+const app=new Koa()
+const Router=require('koa-router')
+const router=new Router()
+const requiredir=require('require-dir')
+const cors=require("koa2-cors")
+
+const routerspath=path.join(__dirname,'api','v1')
+//console.log('routespath: '+routerspath)
+const routersobj=requiredir(routerspath)
+Object.values(routersobj).forEach((item) => {
+  const routerr=item
+  app.use(routerr.routes(),router.allowedMethods())
+});
+const usersRouter = new Router({ prefix: '/api/v1' });
+app.use(koabody())//接受 响应体传来的数据
+app.use(cors())//跨域插件
+app.listen('3000',
+  (err)=>{
+  	if(err){
+  		console.log('服务器失败')
+  	}else{
+  		console.log('服务器启动成功:地址为:http://localhost:3000')
+  	}}
+  )
+
+app.use(router.routes(),router.allowedMethods())
+//app.listen(3000)
