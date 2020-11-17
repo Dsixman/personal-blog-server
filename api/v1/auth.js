4b1f0cd4eae47498ce19d72fc43ac73e29f1ed00
zzhaicn <zhengmurong168@sina.com>
2020-11-17 08:44:32 +0800

    验证用户信息并发送给Token给客户端

--- api/v1/auth.js
+++ api/v1/auth.js
@@ -6,16 +6,17 @@ const User=require("../../model/user.js")
 const jwt=require("jsonwebtoken")
 const router=new Router()
 
-router.get('/auth',(ctx)=>{
+router.post('/auth',(ctx)=>{
     //接收用户名和密码
-  //let body_data=ctx.request.body
-  //let user=body_data.user
-  //let pwd=body_data.pwd
-   dbinit()
-   console.log("aaa")
-   let user='zzh'
-   let pwd='zhenghaihui123'
-   let usertoken={}
+  let body_data=ctx.request.body
+  console.log("body_data:"+body_data)
+  let user=body_data.user
+  let pwd=body_data.password
+  console.log("user:"+user)
+  console.log("password:" +pwd)
+   // let user='zzh'
+   // let pwd='zhenghaihui123'
+  let usertoken={}
   // //数据库验证用户是否存在，密码是否正确
   User.findOne({'name':user,'pwd':pwd},function (err, userdata) {
     if (err){
@@ -27,7 +28,11 @@ router.get('/auth',(ctx)=>{
   //生成token
    const secret='zjdfh3423'
    const token=jwt.sign(usertoken,secret)
-   ctx.body=token
+   let response={
+     token:token,
+     username:user
+   }
+   ctx.body=response
 })
 
 module.exports=router
